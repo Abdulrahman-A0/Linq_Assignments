d03c4014c18a891098182bd1590b02e71c70088f
Abdulrahman-A0 <abdulrahman.ahmed.m98@gmail.com>
2025-08-12 20:51:35 +0300

    Add project files.

---
+++ Linq_01/IntExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_01
+{
+    public static class IntExtensions
+    {
+        public static int Reverse(this int x)
+        {
+            int rem = 0;
+            int rev = 0;
+            while (x > 0)
+            {
+                rem = x % 10;
+                rev = rev * 10 + rem;
+                x /= 10;
+            }
+            return rev;
+        }
+    }
+}
---
+++ Linq_01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.ConstrainedExecution;
+using System.Xml.Linq;
+
+namespace Linq_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            //1.Find all products that are out of stock.
+
+            //var prdoucts = ListGenerator.ProductsList
+            //    .Where(p => p.UnitsInStock == 0);
+
+            //foreach (var product in prdoucts)
+            //    Console.WriteLine(product);
+            #endregion
+
+            #region Q2
+            //2.Find all products that are in stock and cost more than 3.00 per unit.
+
+            //var products = ListGenerator.ProductsList
+            //    .Where(p => p.UnitsInStock > 0 && p.UnitPrice > 3);
+
+            //foreach (var product in products)
+            //    Console.WriteLine(product);
+            #endregion
+
+            #region Q3
+            //3.Returns digits whose name is shorter than their value.
+
+            //String[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            //var result = Arr.Where((d, i) => d.Length < i);
+
+            //foreach (var item in result)
+            //    Console.WriteLine(item);
+            #endregion
+
+            #region Extension Method Example
+            //int x = 64534;
+            //Console.WriteLine(x.Reverse());
+            #endregion
+        }
+    }
+}
